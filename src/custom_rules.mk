860b6c5c0112680926750d42a9f3eed5fa7bfe1c
Ivan Melnychuk <ivan.melnychuk@gmail.com>
2023-12-08 08:05:49 +0100

    van: result

--- src/custom_rules.mk
+++ src/custom_rules.mk
@@ -1,3 +1,21 @@
 # Copyright 2021 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+
+## custom_rules.mk (content)
+MIRYOKU_KMONAD_OS=WIN	# confirmed!
+
+# TODO:
+MIRYOKU_MAPPING=LITE
+# MIRYOKU_MAPPING=TAP
+
+# these are base Alphas for 3 layers
+MIRYOKU_ALPHAS=QWERTY
+MIRYOKU_EXTRA=QWERTY
+MIRYOKU_TAP=QWERTY
+
+# MIRYOKU_NAV=VI
+MIRYOKU_NAV=INVERTEDT	# @NOTE: PgU and PgD are on pinky
+
+# clipboard keys
+MIRYOKU_CLIPBOARD=WIN
